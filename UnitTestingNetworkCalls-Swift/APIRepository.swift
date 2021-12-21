120a75665e60fb11d77d57d879bcd526ec13c8d3
gary.maccabe <gary.maccabe@aix.dev>
2021-12-21 15:57:16 +0000

    Update APIClient with service protocol

--- UnitTestingNetworkCalls-Swift/APIRepository.swift
+++ UnitTestingNetworkCalls-Swift/APIRepository.swift
@@ -7,27 +7,61 @@
 
 import Foundation
 
-class APIRepository {
-    var session: URLSession!
+protocol BeerServiceProtocol {
+  func getBeer(completion: @escaping ([Beer]?, Error?) -> Void) -> URLSessionDataTask
+}
+
+class APIClient: BeerServiceProtocol {
   
-    func getBeer(completion: @escaping ([Beer]?, Error?) -> Void) {
-        guard let url = URL(string: "https://api.punkapi.com/v2/beers") else { fatalError() }
-      
-        session.dataTask(with: url) { (data, response, error) in
-          guard error == nil else {
-            completion(nil, error)
-            return
-          }
-          guard let data = data else {
-            completion(nil, NSError(domain: "no data", code: 10, userInfo: nil))
-            return
-          }
-          do {
-            let beer = try JSONDecoder().decode([Beer].self, from: data)
-            completion(beer, nil)
-          } catch {
-            completion(nil, error)
-          }
-        }.resume()
+  let baseURL: URL
+  let session: URLSession
+  let responseQueue: DispatchQueue?
+  
+  static let shared =
+  APIClient(
+    baseURL: URL(string: "https://api.punkapi.com/v2/")!,
+    session: .shared,
+    responseQueue: .main)
+  
+  init(baseURL: URL,
+       session: URLSession,
+       responseQueue: DispatchQueue?) {
+    self.baseURL = baseURL
+    self.session = session
+    self.responseQueue = responseQueue
+  }
+  
+  func getBeer(completion: @escaping ([Beer]?, Error?) -> Void) -> URLSessionDataTask {
+    let url = URL(string: "beers", relativeTo: baseURL)!
+    let task = session.dataTask(with: url) { [weak self] data, response, error in
+      guard let self = self else { return }
+      guard let response = response as? HTTPURLResponse,
+        response.statusCode == 200,
+        error == nil,
+        let data = data else {
+          self.dispatchResult(error: error, completion: completion)
+          return
+      }
+      let decoder = JSONDecoder()
+      do {
+        let beers = try decoder.decode([Beer].self, from: data)
+        self.dispatchResult(models: beers, completion: completion)
+      } catch {
+        self.dispatchResult(error: error, completion: completion)
+      }
     }
+    task.resume()
+    return task
+  }
+  
+  private func dispatchResult<Type>(models: Type? = nil, error: Error? = nil, completion: @escaping (Type?, Error?) -> Void) {
+        
+    guard let responseQueue = responseQueue else {
+      completion(models, error)
+      return
+    }
+    responseQueue.async {
+      completion(models, error)
+    }
+  }
 }
--- UnitTestingNetworkCalls-Swift/ViewController.swift
+++ UnitTestingNetworkCalls-Swift/ViewController.swift
@@ -9,18 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private var apiRepository = APIRepository()
+    private var apiRepository = APIClient.shared
       
     override func viewDidLoad() {
         super.viewDidLoad()
-        apiRepository = APIRepository()
-        apiRepository.session = URLSession(configuration: .default)
-        apiRepository.getBeer { movies, error in
-          guard let moviesArray = movies else {
-            assert(movies == nil, "beer response is empty")
-            return
-          }
-            print("fetched beer : ", moviesArray[0].name)
+      _ = apiRepository.getBeer() { beers, error in
+            if error == nil {
+                print(beers?.map { $0.name } ?? [])
+            }
         }
     }
 }
